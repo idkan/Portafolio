5f2b1737cf6732bc8aaa9a400b49f6f9e3e46196
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-12 23:03:44 -0600

    Added List

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,16 @@ import { FiHome } from "react-icons/fi";
 
 const Navbar = () => {
     return (
-        <header className="h-10 bg-white">
+        <header className="h-24 bg-white">
             <nav className="flex justify-between items-center h-full">
                 <p className="text-3xl font-bold text-primary">
                     Header Content
                 </p>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog    </a></li>
+                </ul>
             </nav>
         </header>
     );
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-
     return (
         <>
             <Head>
