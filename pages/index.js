745597e9617fdaedc8af33f1ddaaac40b5ad6994
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-14 00:43:15 -0600

    Added Margins

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
 		<Layout>
 			<section id="home" className="pt-8 px-4 pb-16">
 				<div className="max-w-5xl mx-auto grip">
-					<div className="grid">
+					<div className="grid  mb-4">
 						<div className="home__social flex items-center justify-start">
 							<a href="https://www.linkedin.com/in/abraham-serena/" target="_blank" rel="noopener noreferrer"><FiLinkedin className="text-3xl" /></a>
 							<a href="https://github.com/idkan" target="_blank" rel="noopener noreferrer"><FiGithub className="text-3xl" /></a>
@@ -27,11 +27,11 @@ const Home = () => {
 						<div className="home__data">
 							<h1 className="text-3xl font-bold" >Hi, I'm Abraham!</h1>
 							<h3 className="text-xl font-semibold">I'm a Software Engineer</h3>
-							<p className="text-base">I'm a full stack developer with a passion for building web applications. I have a strong background in web development and have worked with a wide range of technologies.</p>
-							<a href="#contact" className="flex items-center">Let's talk <FiSend className="ml-1" /></a>
+							<p className="text-base mb-2">I'm a full stack developer with a passion for building web applications. I have a strong background in web development and have worked with a wide range of technologies.</p>
+							<a href="#contact" className="flex items-center text-secondary">Let's talk <FiSend className="ml-1" /></a>
 						</div>
 					</div>
-					<div className="home__scroll">
+					<div className="home__scroll text-secondary">
 						<a href="#about" className="flex items-center">
 							<BsMouse className="mr-1" />
 							<span className="text-sm font-semibold">Scroll down</span>
