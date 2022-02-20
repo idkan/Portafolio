773425c48ff4b15de5c2532364d39ce1119e0f5c
Abraham Serena <abraham.serena.lk@gmail.com>
2022-02-20 02:56:46 -0600

    Refactor Skills section

--- components/sections/SkillsSection.js
+++ components/sections/SkillsSection.js
@@ -5,7 +5,58 @@ import { BsBraces } from "react-icons/bs";
 
 const SkillsSection = () => {
 
-    const [isFrontOpen, setIsFrontOpen] = useState(true);
+    const [isFrontOpen, setIsFrontOpen] = useState(false);
+
+    const skills = [
+        {
+            title: "Frontend Developer",
+            icon: <BsBraces className="skills__icon text-3xl text-primary mr-3" />,
+            active: true,
+            years: "More than 3 years",
+            technologies: [
+                {
+                    name: "HTML5",
+                    level: "90%",
+                },
+                {
+                    name: "CSS3",
+                    level: "90%",
+                },
+                {
+                    name: "JavaScript",
+                    level: "90%",
+                },
+                {
+                    name: "React",
+                    level: "90%",
+                },
+            ],
+        },
+        {
+            title: "Backend Developer",
+            icon: <FiServer className="skills__icon text-3xl text-primary mr-3" />,
+            active: false,
+            years: "More than 2.5 years",
+            technologies: [
+                {
+                    name: "Node.js",
+                    level: "90%",
+                },
+                {
+                    name: "Express",
+                    level: "90%",
+                },
+                {
+                    name: "MongoDB",
+                    level: "90%",
+                },
+                {
+                    name: "MySQL",
+                    level: "90%",
+                },
+            ],
+        },
+    ];
 
     return (
         <section className="skills section" id="skills">
@@ -13,102 +64,31 @@ const SkillsSection = () => {
             <span className="section__subtitle">My Technical Skills</span>
             <div className="skills__container container grid gap-6 gap-y-0">
                 <div className="skills__data col-span-1/3">
-                    <div className="skills__content" onClick={() => setIsFrontOpen(!isFrontOpen)}>
-                        <div className="skills__header flex items-center mb-10 cursor-pointer">
-                            <BsBraces className="skills__icon text-3xl text-primary mr-3" />
-                            <div>
-                                <h1 className="skills__title text-xl">Frontend Developer</h1>
-                                <span className="skills__subtitle text-base text-text-light font-medium mb-3">More than 3 years</span>
+                    {skills.map((item, index) => (
+                        <div className="skills__content" onClick={() => setIsFrontOpen(!index.active)} key={index}>
+                            <div className="skills__header flex items-center mb-10 cursor-pointer">
+                                {item.icon}
+                                <div>
+                                    <h1 className="skills__title text-xl">{item.title}</h1>
+                                    <span className="skills__subtitle text-base text-text-light font-medium mb-3">{item.years}</span>
+                                </div>
+                                <FiChevronDown className={`skills__arrow text-3xl text-primary ml-auto duration-[400ms]  ${isFrontOpen ? "rotate-[-180deg]" : ""}`} />
                             </div>
-                            <FiChevronDown className={`skills__arrow text-3xl text-primary ml-auto duration-[400ms]  ${isFrontOpen ? "rotate-[-180deg]" : ""}`} />
-                        </div>
-                        <div className={`skills__list grid gap-6 ${isFrontOpen ? "h-max mb-10" : "h-0 overflow-hidden"}`}>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">HTML</h3>
-                                    <span className="skills__number">90%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__html" style={{ width: "90%" }} />
-                                </div>
-                            </div>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">CSS</h3>
-                                    <span className="skills__number">80%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__css" style={{ width: "80%" }} />
-                                </div>
-                            </div>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">JavaScript</h3>
-                                    <span className="skills__number">90%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__javascript" style={{ width: "90%" }} />
-                                </div>
-                            </div>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">React</h3>
-                                    <span className="skills__number">60%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__react" style={{ width: "60%" }} />
-                                </div>
+                            <div className={`skills__list grid gap-6 ${isFrontOpen ? "h-max mb-10" : "h-0 overflow-hidden"}`}>
+                                {item.technologies.map((tech, index) => (
+                                    <div className="skills__data" key={index}>
+                                        <div className="skills__titles flex justify-between mb-2">
+                                            <h3 className="skills__name text-base font-medium">{tech.name}</h3>
+                                            <span className="skills__number">{tech.level}</span>
+                                        </div>
+                                        <div className="skills__bar h-1 rounded-md bg-text-light">
+                                            <span className="skills__percentage h-1 rounded-md block bg-primary skills__html" style={{ width: tech.level }} />
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
                         </div>
-                    </div>
-                    <div className="skills__content" onClick={() => setIsFrontOpen(!isFrontOpen)}>
-                        <div className="skills__header flex items-center mb-10 cursor-pointer">
-                            <FiServer className="skills__icon text-3xl text-primary mr-3" />
-                            <div>
-                                <h1 className="skills__title text-xl">Backend Developer</h1>
-                                <span className="skills__subtitle text-base text-text-light font-medium mb-3">More than 2.5 years</span>
-                            </div>
-                            <FiChevronDown className={`skills__arrow text-3xl text-primary ml-auto duration-[400ms]  ${isFrontOpen ? "" : "rotate-[-180deg]"}`} />
-                        </div>
-                        <div className={`skills__list grid gap-6 ${isFrontOpen ? "h-0 overflow-hidden" : "h-max mb-10"}`}>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">NodeJS</h3>
-                                    <span className="skills__number">80%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__node" style={{ width: "80%" }} />
-                                </div>
-                            </div>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">PHP</h3>
-                                    <span className="skills__number">70%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__php" style={{ width: "70%" }} />
-                                </div>
-                            </div>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">Java</h3>
-                                    <span className="skills__number">60%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__java" style={{ width: "60%" }} />
-                                </div>
-                            </div>
-                            <div className="skills__data">
-                                <div className="skills__titles flex justify-between mb-2">
-                                    <h3 className="skills__name text-base font-medium">Python</h3>
-                                    <span className="skills__number">60%</span>
-                                </div>
-                                <div className="skills__bar h-1 rounded-md bg-text-light">
-                                    <span className="skills__percentage h-1 rounded-md block bg-primary skills__python" style={{ width: "55%" }} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
