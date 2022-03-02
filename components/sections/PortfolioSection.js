2db3874291ea8b1d46a32c36b4b875f8c8290aa3
Abraham Serena <abraham.serena.lk@gmail.com>
2022-03-01 23:21:58 -0600

    Added Swiper Styles

--- components/sections/PortfolioSection.js
+++ components/sections/PortfolioSection.js
@@ -15,6 +15,7 @@ const PortfolioSection = () => {
         pagination: true,
         mousewheel: true,
         keyboard: true,
+        loop: true,
         modules: [Navigation, Pagination, Mousewheel, Keyboard],
         className: 'portfolio-swiper'
     };
@@ -24,18 +25,17 @@ const PortfolioSection = () => {
             <h2 className="section__title">Portfolio</h2>
             <span className="section__subtitle">My recent work</span>
 
-            <div className="portfolio__container container">
-                <Swiper {...swiperParams} >
+            <div className="portfolio__container container h-80">
+                <Swiper {...swiperParams} className="h-96">
                     <SwiperSlide className="portfolio__content grid px-6 py-0">
                         <img src="./assets/images/rentMe.png" alt="Rent Me APP" className="portfolio__image w-64 rounded-lg justify-self-center" />
                         <div className="portfolio__data">
-                            <h3 className="portfolio__title text-lg font-medium mb-2">Rent Me APP</h3>
-                            <p className="portfolio__description mb-3">
-                                RentMe is a web application that allows users to Rent or Buy a property.
+                            <h3 className="portfolio__title text-lg font-medium mb-2">RentMe</h3>
+                            <p className="portfolio__description font-normal mb-3">
+                                RentMe is a web app that allows users to Rent or Buy a property.
                                 With an advanced search feature, users can find the property they are looking for.
-                                It full responsive and mobile friendly.
                             </p>
-                            <a href="#" className="button bg-primary text-white p-4 rounded-lg font-medium hover:bg-primary-alt button--flex inline-flex items-center button--small portfolio__button group">
+                            <a href="#" className="button bg-primary text-white p-3 rounded-lg font-normal hover:bg-primary-alt button--flex inline-flex items-center button--small portfolio__button group">
                                 View Project
                                 <BsArrowRight className="button__icon ml-2 group-hover:translate-x-1" />
                             </a>
@@ -45,12 +45,12 @@ const PortfolioSection = () => {
                     <SwiperSlide className="portfolio__content grid px-6 py-0">
                         <img src="./assets/images/cryptoWorld.png" alt="Crypto World APP" className="portfolio__image w-64 rounded-lg justify-self-center" />
                         <div className="portfolio__data">
-                            <h3 className="portfolio__title text-lg font-medium mb-2">Crypto World</h3>
-                            <p className="portfolio__description">
-                                Crypto World is a web application that allows users to see the current price of the most famous cryptocurrencies in the world, all in one place.
-                                Allowing users to compare the price and see the latest news about the cryptocurrencies.
+                            <h3 className="portfolio__title text-lg font-medium mb-2">CryptoWorld</h3>
+                            <p className="portfolio__description font-normal mb-3">
+                                CryptoWorld is a web app that allows users to see cryptocurrencies information.
+                                Allowing users to news and prices about the cryptocurrencies.
                             </p>
-                            <a href="#" className="button bg-primary text-white p-4 rounded-lg font-medium hover:bg-primary-alt button--flex inline-flex items-center button--small portfolio__button group">
+                            <a href="#" className="button bg-primary text-white p-3 rounded-lg font-normal hover:bg-primary-alt button--flex inline-flex items-center button--small portfolio__button group">
                                 View Project
                                 <BsArrowRight className="button__icon ml-2 group-hover:translate-x-1" />
                             </a>
@@ -61,11 +61,11 @@ const PortfolioSection = () => {
                         <img src="./assets/images/travely.png" alt="Portfolio 2" className="portfolio__image w-64 rounded-lg justify-self-center" />
                         <div className="portfolio__data">
                             <h3 className="portfolio__title text-lg font-medium mb-2">Travely</h3>
-                            <p className="portfolio__description">
-                                Travely is a web application that allows users to search for a destination and see the most popular places in that destination.
+                            <p className="portfolio__description font-normal mb-3">
+                                Travely is a web app that allows users to search for a destination and see the most popular places.
                                 Food, hotels, activities, and more. All in one place.
                             </p>
-                            <a href="#" className="button bg-primary text-white p-4 rounded-lg font-medium hover:bg-primary-alt button--flex inline-flex items-center button--small portfolio__button group">
+                            <a href="#" className="button bg-primary text-white p-3 rounded-lg font-normal hover:bg-primary-alt button--flex inline-flex items-center button--small portfolio__button group">
                                 View Project
                                 <BsArrowRight className="button__icon ml-2 group-hover:translate-x-1" />
                             </a>
