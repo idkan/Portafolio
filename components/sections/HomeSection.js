9cafadf3a7fcfc39fbf7feaf2369c215c19ca734
Abraham Serena <abraham.serena.lk@gmail.com>
2022-03-10 00:32:11 -0600

    Added Home responsive design xs

--- components/sections/HomeSection.js
+++ components/sections/HomeSection.js
@@ -4,8 +4,8 @@ import { BsMouse, BsArrowDown } from "react-icons/bs";
 const HomeSection = () => {
     return (
         <section className="home section bg-body dark:bg-body-dark" id="home">
-            <div className="home__container container grid gap-4">
-                <div className="home__content grid gap-6 grid-cols-[.5fr_3fr] pt-14 items-center">
+            <div className="home__container container grid gap-4 xs:mx-4">
+                <div className="home__content grid gap-6 grid-cols-[.5fr_3fr] pt-14 items-center xs:grid-cols-[.25fr_3fr]">
                     <div className="home__social grid grid-cols-[max-content] gap-y-4">
                         <a href="https://www.linkedin.com/in/abraham-serena/" className="home__social-icon text-lg text-primary hover:text-primary-alt" target="_blank" rel="noopener noreferrer"><FiLinkedin className="text-xl inline-block" /></a>
                         <a href="https://github.com/idkan/" className="home__social-icon text-lg text-primary hover:text-primary-alt" target="_blank" rel="noopener noreferrer"><FiGithub className="text-xl inline-block" /></a>
@@ -18,7 +18,7 @@ const HomeSection = () => {
                             </mask>
                             <g mask="url(#mask0)">
                                 <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z" />
-                                <image className="home__blob-img w-48" x="12" y="18" href="./assets/images/profile.png" />
+                                <image className="home__blob-img w-48 xs:w-44" x="12" y="18" href="./assets/images/profile.png" />
                             </g>
                         </svg>
                     </div>
