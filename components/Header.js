f782b4296fd4a941527a7fa65041ccc78a0496ab
Abraham Serena <abraham.serena.lk@gmail.com>
2022-03-17 00:32:42 -0600

    Added responsive design MD to header

--- components/Header.js
+++ components/Header.js
@@ -20,11 +20,11 @@ const Navbar = () => {
 
         if (currentTheme === "light") {
             return (
-                <FiMoon className="text-xl cursor-pointer text-sm text-title mr-4 hover:text-primary-alt duration-300 dark:text-title-dark dark:hover:text-primary" onClick={() => setTheme("dark")} />
+                <FiMoon className="text-xl cursor-pointer text-sm text-title mr-4 hover:text-primary-alt duration-300 dark:text-title-dark dark:hover:text-primary md:m-0" onClick={() => setTheme("dark")} />
             )
         } else {
             return (
-                <FiSun className="text-xl cursor-pointer text-sm text-title mr-4 hover:text-primary-alt duration-300 dark:text-title-dark dark:hover:text-primary" onClick={() => setTheme("light")} />
+                <FiSun className="text-xl cursor-pointer text-sm text-title mr-4 hover:text-primary-alt duration-300 dark:text-title-dark dark:hover:text-primary md:m-0" onClick={() => setTheme("light")} />
             )
         }
     }
@@ -66,23 +66,23 @@ const Navbar = () => {
     });
 
     return (
-        <header className="header dark:bg-body-dark" id="header">
+        <header className="header dark:bg-body-dark md:top-0 md:bottom-[initial] md:py-0 md:px-4" id="header">
             <nav className="nav max-w-5xl h-12 flex justify-between items-center container xs:mx-4 md:mx-auto">
                 <a href="#" className="nav__logo text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary">Abraham </a>
-                <div id="nav-menu" className={`nav__menu fixed ${navMenuOpen ? "bottom-0" : "-bottom-full"} left-0 w-full bg-body pt-8 px-6 pb-12 shadow-[0_-1px_4px_rgba(0,0,0,0.15)] rounded-t-3xl rounded-b-sm duration-300 dark:bg-body-dark xs:px-1 xs:pb-16`}>
-                    <ul className="nav__list grid grid-cols-3 gap-8 xs:gap-x-0">
-                        <li className="nav__item" onClick={toggleNavMenu}><a href="#home" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiHome className="text-xl" />Home</a></li>
-                        <li className="nav__item" onClick={toggleNavMenu}><a href="#about" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiUser className="text-xl" />About</a></li>
-                        <li className="nav__item" onClick={toggleNavMenu}><a href="#skills" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiFileText className="text-xl" />Skills</a></li>
-                        <li className="nav__item" onClick={toggleNavMenu}><a href="#portfolio" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiBriefcase className="text-xl" />Projects</a></li>
-                        <li className="nav__item" onClick={toggleNavMenu}><a href="#blog" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiBook className="text-xl" />Blog</a></li>
-                        <li className="nav__item" onClick={toggleNavMenu}><a href="#contact" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiMessageSquare className="text-xl" />Contact</a></li>
+                <div id="nav-menu" className={`nav__menu fixed ${navMenuOpen ? "bottom-0" : "-bottom-full"} left-0 w-full bg-body pt-8 px-6 pb-12 shadow-[0_-1px_4px_rgba(0,0,0,0.15)] rounded-t-3xl rounded-b-sm duration-300 dark:bg-body-dark xs:px-1 xs:pb-16 md:top-0 md:bottom-[initial] md:h-[4.5rem] md:gap-x-4 md:ml-auto`}>
+                    <ul className="nav__list grid grid-cols-3 gap-8 xs:gap-x-0 md:flex md:gap-x-8">
+                        <li className="nav__item" onClick={toggleNavMenu}><a href="#home" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiHome className="text-xl md:hidden" />Home</a></li>
+                        <li className="nav__item" onClick={toggleNavMenu}><a href="#about" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiUser className="text-xl md:hidden" />About</a></li>
+                        <li className="nav__item" onClick={toggleNavMenu}><a href="#skills" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiFileText className="text-xl md:hidden" />Skills</a></li>
+                        <li className="nav__item" onClick={toggleNavMenu}><a href="#portfolio" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiBriefcase className="text-xl md:hidden" />Projects</a></li>
+                        <li className="nav__item" onClick={toggleNavMenu}><a href="#blog" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiBook className="text-xl md:hidden" />Blog</a></li>
+                        <li className="nav__item" onClick={toggleNavMenu}><a href="#contact" className="nav__link flex flex-col items-center text-tiny text-title font-medium hover:text-primary dark:text-title-dark dark:hover:text-primary"><FiMessageSquare className="text-xl md:hidden" />Contact</a></li>
                     </ul>
-                    <FiX id="nav-close" className="nav__close absolute right-5 bottom-2 text-2xl cursor-pointer text-primary hover:text-primary-alt" onClick={toggleNavMenu} />
+                    <FiX id="nav-close" className="nav__close absolute right-5 bottom-2 text-2xl cursor-pointer text-primary hover:text-primary-alt md:hidden" onClick={toggleNavMenu} />
                 </div>
                 <div className="nav__btns flex items-center">
                     {renderThemeChanger()}
-                    <div id="nav-toggle" className="nav__toggle text-title font-medium text-lg cursor-pointer hover:text-primary dark:text-title-dark dark:hover:text-primary" onClick={toggleNavMenu}>
+                    <div id="nav-toggle" className="nav__toggle text-title font-medium text-lg cursor-pointer hover:text-primary dark:text-title-dark dark:hover:text-primary md:hidden" onClick={toggleNavMenu}>
                         <FiGrid className="font-bold" />
                     </div>
                 </div>
