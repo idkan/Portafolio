993d8ee8841e42232dc5a6b81922b8524db040d1
Abraham Serena <abraham.serena.lk@gmail.com>
2022-03-17 00:32:35 -0600

    Added responsive design MD to footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,13 +3,13 @@ import { FiGithub, FiTwitter, FiInstagram } from "react-icons/fi";
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer__bg bg-secondary pt-8 px-0 pb-12 dark:bg-secondary-dark">
-                <div className="footer__container container grid gap-6 gap-y-14 xs:mx-4 sm:grid-cols-[repeat(2,_1fr)] md:mx-auto">
+            <div className="footer__bg bg-secondary pt-8 px-0 pb-12 dark:bg-secondary-dark md:pt-12 md:px-0 md:pb-14">
+                <div className="footer__container container grid gap-6 gap-y-14 xs:mx-4 sm:grid-cols-[repeat(2,_1fr)] md:mx-auto md:py-0 md:px-4 md:grid-cols-[repeat(3,_1fr)]">
                     <div className="footer__item">
                         <h1 className="footer__title text-lg mb-1 text-white">Abraham</h1>
                         <span className="footer__subtitle text-sm text-white">Software Engineer</span>
                     </div>
-                    <ul className="footer__links flex flex-col gap-y-6 text-white">
+                    <ul className="footer__links flex flex-col gap-y-6 text-white md:flex-row md:gap-x-8">
                         <li className="footer__links hover:text-primary-light">
                             <a href="#about" className="footer__links-item"> About</a>
                         </li>
@@ -20,7 +20,7 @@ const Footer = () => {
                             <a href="#" className="footer__links-item"> Blog</a>
                         </li>
                     </ul>
-                    <div className="footer__socials flex text-white">
+                    <div className="footer__socials flex text-white md:justify-self-end">
                         <a href="https://github.com/idkan" target="_blank" className="footer__socials-item text-sm mr-6 hover:text-primary-light">
                             <FiGithub className="inline-block" />
                         </a>
@@ -32,7 +32,7 @@ const Footer = () => {
                         </a>
                     </div>
                 </div>
-                <p className="footer__copyright text-xs text-center text-primary-light mt-12"> &copy; 2022 ABSE Dev. All rights reserved. </p>
+                <p className="footer__copyright text-xs text-center text-primary-light mt-12 md:mt-[4.5rem]"> &copy; 2022 ABSE Dev. All rights reserved. </p>
             </div>
         </footer>
     );
