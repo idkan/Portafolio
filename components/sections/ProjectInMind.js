948b219b35dfba90c384f19b60ee94abd104ecc5
Abraham Serena <abraham.serena.lk@gmail.com>
2022-03-18 18:26:10 -0600

    Fixed Padding

--- components/sections/ProjectInMind.js
+++ components/sections/ProjectInMind.js
@@ -2,7 +2,7 @@ import { FiSend } from "react-icons/fi";
 
 const ProjectInMind = () => {
     return (
-        <section className="project section text-center md:pt-8 md:px-0 md:pb-8 md:text-left">
+        <section className="project section text-center md:pt-24 md:px-0 md:pb-8 md:text-left">
             <div className="project__bg bg-primary py-12 dark:bg-body-dark md:bg-body">
                 <div className="project__container container grid gap-6 xs:mx-4 sm:grid-cols-[repeat(2,_1fr)] md:mx-auto md:bg-secondary md:rounded-2xl md:pt-12 md:px-10 md:pb-0 md:grid-cols-[1fr_max-content] md:gap-x-12">
                     <div className="project__data md:pt-[.8rem]">
